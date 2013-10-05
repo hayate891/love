15b44f76da739dd8213e849e3f8646e51d2d6305
Alex Szpakowski <slime73@gmail.com>
2013-10-05 01:54:41 -0300

    Added joystick vibration support for most common controllers (resolves issue #159).
    Added Joystick:setVibration(left, right), Joystick:getVibration, and Joystick:isVibrationSupported.
    left and right are numbers between 0 and 1 which correspond to the strength of the gamepad's left and right rumble motors, if it has them.

---
+++ src/modules/graphics/opengl/Image.cpp
@@ -0,0 +1,747 @@
+/**
+ * Copyright (c) 2006-2013 LOVE Development Team
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ **/
+
+#include "Image.h"
+
+// STD
+#include <cstring> // For memcpy
+#include <algorithm> // for min/max
+
+namespace love
+{
+namespace graphics
+{
+namespace opengl
+{
+
+float Image::maxMipmapSharpness = 0.0f;
+
+Image::FilterMode Image::defaultMipmapFilter = Image::FILTER_NONE;
+float Image::defaultMipmapSharpness = 0.0f;
+
+Image::Image(love::image::ImageData *data)
+	: data(data)
+	, cdata(0)
+	, width((float)(data->getWidth()))
+	, height((float)(data->getHeight()))
+	, texture(0)
+	, mipmapSharpness(defaultMipmapSharpness)
+	, mipmapsCreated(false)
+	, compressed(false)
+	, usingDefaultTexture(false)
+{
+	data->retain();
+	preload();
+}
+
+Image::Image(love::image::CompressedData *cdata)
+	: data(0)
+	, cdata(cdata)
+	, width((float)(cdata->getWidth(0)))
+	, height((float)(cdata->getHeight(0)))
+	, texture(0)
+	, mipmapSharpness(defaultMipmapSharpness)
+	, mipmapsCreated(false)
+	, compressed(true)
+	, usingDefaultTexture(false)
+{
+	cdata->retain();
+	preload();
+}
+
+Image::~Image()
+{
+	if (data != 0)
+		data->release();
+	if (cdata != 0)
+		cdata->release();
+	unload();
+}
+
+float Image::getWidth() const
+{
+	return width;
+}
+
+float Image::getHeight() const
+{
+	return height;
+}
+
+const Vertex *Image::getVertices() const
+{
+	return vertices;
+}
+
+love::image::ImageData *Image::getImageData() const
+{
+	return data;
+}
+
+love::image::CompressedData *Image::getCompressedData() const
+{
+	return cdata;
+}
+
+void Image::draw(float x, float y, float angle, float sx, float sy, float ox, float oy, float kx, float ky) const
+{
+	static Matrix t;
+
+	t.setTransformation(x, y, angle, sx, sy, ox, oy, kx, ky);
+	drawv(t, vertices);
+}
+
+void Image::drawg(love::graphics::Geometry *geom, float x, float y, float angle, float sx, float sy, float ox, float oy, float kx, float ky) const
+{
+	static Matrix t;
+	t.setTransformation(x, y, angle, sx, sy, ox, oy, kx, ky);
+
+	const Vertex *v = geom->getVertexArray();
+	size_t vertcount = geom->getVertexCount();
+
+	// Padded NPOT images require texture coordinate scaling with Geometry.
+	if (!hasNpot())
+		v = scaleNPOT(v, vertcount);
+
+	// use colors stored in geometry (horrible, horrible hack)
+	if (geom->hasVertexColors())
+	{
+		glEnableClientState(GL_COLOR_ARRAY);
+		glColorPointer(4, GL_UNSIGNED_BYTE, sizeof(Vertex), (GLvoid *) &v[0].r);
+	}
+
+	GLenum glmode;
+	switch (geom->getDrawMode())
+	{
+	case Geometry::DRAW_MODE_FAN:
+	default:
+		glmode = GL_TRIANGLE_FAN;
+		break;
+	case Geometry::DRAW_MODE_STRIP:
+		glmode = GL_TRIANGLE_STRIP;
+		break;
+	case Geometry::DRAW_MODE_TRIANGLES:
+		glmode = GL_TRIANGLES;
+		break;
+	}
+
+	drawv(t, v, vertcount, glmode, geom->getElementArray(), geom->getElementCount());
+
+	if (geom->hasVertexColors())
+	{
+		glDisableClientState(GL_COLOR_ARRAY);
+		gl.setColor(gl.getColor());
+	}
+
+	// If we made new verts with scaled texcoords then we should clean them up.
+	if (!hasNpot())
+		delete[] v;
+}
+
+void Image::uploadCompressedMipmaps()
+{
+	if (!isCompressed() || !cdata || !hasCompressedTextureSupport(cdata->getFormat()))
+		return;
+
+	bind();
+
+	int mipmapcount = cdata->getMipmapCount();
+
+	// We have to inform OpenGL if the image doesn't have all mipmap levels.
+	if (GLEE_VERSION_1_2 || GLEE_SGIS_texture_lod)
+	{
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, mipmapcount - 1);
+	}
+	else if (cdata->getWidth(mipmapcount-1) > 1 || cdata->getHeight(mipmapcount-1) > 1)
+	{
+		// Telling OpenGL to ignore certain levels isn't always supported.
+		throw love::Exception("Cannot load mipmaps: "
+		      "compressed image does not have all required levels.");
+	}
+
+	for (int i = 1; i < mipmapcount; i++)
+	{
+		glCompressedTexImage2DARB(GL_TEXTURE_2D,
+		                          i,
+		                          getCompressedFormat(cdata->getFormat()),
+		                          cdata->getWidth(i),
+		                          cdata->getHeight(i),
+		                          0,
+		                          GLsizei(cdata->getSize(i)),
+		                          cdata->getData(i));
+	}
+}
+
+void Image::createMipmaps()
+{
+	// Only valid for Images created with ImageData.
+	if (!data)
+		return;
+
+	if (!hasMipmapSupport())
+		throw love::Exception("Mipmap filtering is not supported on this system.");
+
+	// Some old drivers claim support for NPOT textures, but fail when creating
+	// mipmaps. We can't detect which systems will do this, so we fail gracefully
+	// for all NPOT images.
+	int w = int(width), h = int(height);
+	if (w != next_p2(w) || h != next_p2(h))
+	{
+		throw love::Exception("Cannot create mipmaps: "
+		      "image does not have power of two dimensions.");
+	}
+
+	bind();
+
+	// Prevent other threads from changing the ImageData while we upload it.
+	love::thread::Lock lock(data->getMutex());
+
+	if (hasNpot() && (GLEE_VERSION_3_0 || GLEE_ARB_framebuffer_object))
+	{
+		// AMD/ATI drivers have several bugs when generating mipmaps,
+		// re-uploading the entire base image seems to be required.
+		glTexImage2D(GL_TEXTURE_2D,
+		             0,
+		             GL_RGBA8,
+		             (GLsizei)width,
+		             (GLsizei)height,
+		             0,
+		             GL_RGBA,
+		             GL_UNSIGNED_BYTE,
+		             data->getData());
+
+		// More bugs: http://www.opengl.org/wiki/Common_Mistakes#Automatic_mipmap_generation
+		glEnable(GL_TEXTURE_2D);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE);
+		glTexSubImage2D(GL_TEXTURE_2D,
+		                0,
+		                0,
+		                0,
+		                (GLsizei)width,
+		                (GLsizei)height,
+		                GL_RGBA,
+		                GL_UNSIGNED_BYTE,
+		                data->getData());
+	}
+}
+
+void Image::checkMipmapsCreated()
+{
+	if (mipmapsCreated || filter.mipmap == FILTER_NONE || usingDefaultTexture)
+		return;
+
+	if (isCompressed() && cdata && hasCompressedTextureSupport(cdata->getFormat()))
+		uploadCompressedMipmaps();
+	else if (data)
+		createMipmaps();
+	else
+		return;
+
+	mipmapsCreated = true;
+}
+
+void Image::setFilter(const Image::Filter &f)
+{
+	filter = f;
+
+	// We don't want filtering or (attempted) mipmaps on the default texture.
+	if (usingDefaultTexture)
+	{
+		filter.mipmap = FILTER_NONE;
+		filter.min = filter.mag = FILTER_NEAREST;
+	}
+
+	bind();
+	filter.anisotropy = gl.setTextureFilter(filter);
+	checkMipmapsCreated();
+}
+
+const Image::Filter &Image::getFilter() const
+{
+	return filter;
+}
+
+void Image::setWrap(const Image::Wrap &w)
+{
+	wrap = w;
+
+	bind();
+	gl.setTextureWrap(w);
+}
+
+const Image::Wrap &Image::getWrap() const
+{
+	return wrap;
+}
+
+void Image::setMipmapSharpness(float sharpness)
+{
+	if (hasMipmapSharpnessSupport())
+	{
+		// LOD bias has the range (-maxbias, maxbias)
+		mipmapSharpness = std::min(std::max(sharpness, -maxMipmapSharpness + 0.01f), maxMipmapSharpness - 0.01f);
+
+		bind();
+
+		// negative bias is sharper
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, -mipmapSharpness);
+	}
+	else
+		mipmapSharpness = 0.0f;
+}
+
+float Image::getMipmapSharpness() const
+{
+	return mipmapSharpness;
+}
+
+void Image::bind() const
+{
+	if (texture == 0)
+		return;
+
+	gl.bindTexture(texture);
+}
+
+void Image::preload()
+{
+	memset(vertices, 255, sizeof(Vertex)*4);
+
+	vertices[0].x = 0;
+	vertices[0].y = 0;
+	vertices[1].x = 0;
+	vertices[1].y = height;
+	vertices[2].x = width;
+	vertices[2].y = height;
+	vertices[3].x = width;
+	vertices[3].y = 0;
+
+	vertices[0].s = 0;
+	vertices[0].t = 0;
+	vertices[1].s = 0;
+	vertices[1].t = 1;
+	vertices[2].s = 1;
+	vertices[2].t = 1;
+	vertices[3].s = 1;
+	vertices[3].t = 0;
+
+	filter = getDefaultFilter();
+	filter.mipmap = defaultMipmapFilter;
+}
+
+bool Image::load()
+{
+	return loadVolatile();
+}
+
+void Image::unload()
+{
+	return unloadVolatile();
+}
+
+bool Image::loadVolatile()
+{
+	if (isCompressed() && cdata && !hasCompressedTextureSupport(cdata->getFormat()))
+	{
+		const char *str;
+		if (image::CompressedData::getConstant(cdata->getFormat(), str))
+		{
+			throw love::Exception("Cannot create image: "
+			      "%s compressed images are not supported on this system.", str);
+		}
+		else
+			throw love::Exception("cannot create image: format is not supported on this system.");
+	}
+
+	if (hasMipmapSharpnessSupport() && maxMipmapSharpness == 0.0f)
+		glGetFloatv(GL_MAX_TEXTURE_LOD_BIAS, &maxMipmapSharpness);
+
+	if (hasNpot())
+		return loadVolatileNPOT();
+	else
+		return loadVolatilePOT();
+}
+
+bool Image::loadVolatilePOT()
+{
+	glGenTextures(1,(GLuint *)&texture);
+	gl.bindTexture(texture);
+
+	filter.anisotropy = gl.setTextureFilter(filter);
+	gl.setTextureWrap(wrap);
+	setMipmapSharpness(mipmapSharpness);
+
+	float p2width = next_p2(width);
+	float p2height = next_p2(height);
+	float s = width/p2width;
+	float t = height/p2height;
+
+	// Use a default texture if the size is too big for the system.
+	if (p2width > gl.getMaxTextureSize() || p2height > gl.getMaxTextureSize())
+	{
+		uploadDefaultTexture();
+		return true;
+	}
+
+	vertices[1].t = t;
+	vertices[2].t = t;
+	vertices[2].s = s;
+	vertices[3].s = s;
+
+	// We want this lock to potentially cover mipmap creation as well.
+	love::thread::EmptyLock lock;
+
+	while (glGetError() != GL_NO_ERROR); // clear errors
+
+	if (isCompressed() && cdata)
+	{
+		if (s < 1.0f || t < 1.0f)
+		{
+			throw love::Exception("Cannot create image: "
+				  "compressed NPOT images are not supported on this system.");
+		}
+
+		glCompressedTexImage2DARB(GL_TEXTURE_2D,
+		                          0,
+		                          getCompressedFormat(cdata->getFormat()),
+		                          cdata->getWidth(0),
+		                          cdata->getHeight(0),
+		                          0,
+		                          GLsizei(cdata->getSize(0)),
+		                          cdata->getData(0));
+	}
+	else if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D,
+		             0,
+		             GL_RGBA8,
+		             (GLsizei)p2width,
+		             (GLsizei)p2height,
+		             0,
+		             GL_RGBA,
+		             GL_UNSIGNED_BYTE,
+		             0);
+
+		lock.setLock(data->getMutex());
+		glTexSubImage2D(GL_TEXTURE_2D,
+		                0,
+		                0, 0,
+		                (GLsizei)width,
+		                (GLsizei)height,
+		                GL_RGBA,
+		                GL_UNSIGNED_BYTE,
+		                data->getData());
+	}
+
+	GLenum glerr = glGetError();
+	if (glerr != GL_NO_ERROR)
+		throw love::Exception("Cannot create image (error code 0x%x)", glerr);
+
+	usingDefaultTexture = false;
+	mipmapsCreated = false;
+	checkMipmapsCreated();
+
+	return true;
+}
+
+bool Image::loadVolatileNPOT()
+{
+	glGenTextures(1,(GLuint *)&texture);
+	gl.bindTexture(texture);
+
+	filter.anisotropy = gl.setTextureFilter(filter);
+	gl.setTextureWrap(wrap);
+	setMipmapSharpness(mipmapSharpness);
+
+	// Use a default texture if the size is too big for the system.
+	if (width > gl.getMaxTextureSize() || height > gl.getMaxTextureSize())
+	{
+		uploadDefaultTexture();
+		return true;
+	}
+
+	// We want this lock to potentially cover mipmap creation as well.
+	love::thread::EmptyLock lock;
+
+	while (glGetError() != GL_NO_ERROR); // clear errors
+
+	if (isCompressed() && cdata)
+	{
+		GLenum format = getCompressedFormat(cdata->getFormat());
+		glCompressedTexImage2DARB(GL_TEXTURE_2D,
+		                          0,
+		                          format,
+		                          cdata->getWidth(0),
+		                          cdata->getHeight(0),
+		                          0,
+		                          GLsizei(cdata->getSize(0)),
+		                          cdata->getData(0));
+	}
+	else if (data)
+	{
+		lock.setLock(data->getMutex());
+		glTexImage2D(GL_TEXTURE_2D,
+		             0,
+		             GL_RGBA8,
+		             (GLsizei)width,
+		             (GLsizei)height,
+		             0,
+		             GL_RGBA,
+		             GL_UNSIGNED_BYTE,
+		             data->getData());
+	}
+
+	GLenum glerr = glGetError();
+	if (glerr != GL_NO_ERROR)
+		throw love::Exception("Cannot create image (error code 0x%x)", glerr);
+
+	usingDefaultTexture = false;
+	mipmapsCreated = false;
+	checkMipmapsCreated();
+
+	return true;
+}
+
+void Image::unloadVolatile()
+{
+	// Delete the hardware texture.
+	if (texture != 0)
+	{
+		gl.deleteTexture(texture);
+		texture = 0;
+	}
+}
+
+bool Image::refresh()
+{
+	// No effect if the texture hasn't been created yet.
+	if (texture == 0)
+		return false;
+
+	while (glGetError() != GL_NO_ERROR); // clear errors
+
+	// We want this lock to potentially cover mipmap creation as well.
+	love::thread::EmptyLock lock;
+
+	bind();
+
+	if (isCompressed() && cdata)
+	{
+		GLenum format = getCompressedFormat(cdata->getFormat());
+		glCompressedTexSubImage2DARB(GL_TEXTURE_2D,
+		                             0,
+		                             0, 0,
+		                             cdata->getWidth(0),
+		                             cdata->getHeight(0),
+		                             format,
+		                             GLsizei(cdata->getSize(0)),
+		                             cdata->getData(0));
+	}
+	else if (data)
+	{
+		lock.setLock(data->getMutex());
+		glTexSubImage2D(GL_TEXTURE_2D,
+		                0,
+		                0, 0,
+		                (GLsizei)width,
+		                (GLsizei)height,
+		                GL_RGBA,
+		                GL_UNSIGNED_BYTE,
+		                data->getData());
+	}
+
+	if (glGetError() != GL_NO_ERROR)
+		uploadDefaultTexture();
+	else
+		usingDefaultTexture = false;
+
+	mipmapsCreated = false;
+	checkMipmapsCreated();
+
+	return true;
+}
+
+void Image::uploadDefaultTexture()
+{
+	usingDefaultTexture = true;
+
+	bind();
+	setFilter(filter);
+
+	// A nice friendly checkerboard to signify invalid textures...
+	GLubyte px[] = {0xFF,0xFF,0xFF,0xFF, 0xC0,0xC0,0xC0,0xFF,
+	                0xC0,0xC0,0xC0,0xFF, 0xFF,0xFF,0xFF,0xFF};
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 2, 2, 0, GL_RGBA, GL_UNSIGNED_BYTE, px);
+}
+
+love::Vector Image::getTexCoordScale() const
+{
+	// FIXME: this should be changed if Image::loadVolatilePOT changes.
+	return love::Vector(vertices[2].s, vertices[2].t);
+}
+
+Vertex *Image::scaleNPOT(const love::Vertex *v, size_t count) const
+{
+	Vertex *newverts = new Vertex[count];
+	love::Vector scale = getTexCoordScale();
+
+	for (size_t i = 0; i < count; i++)
+	{
+		newverts[i] = v[i];
+		newverts[i].s *= scale.x;
+		newverts[i].t *= scale.y;
+	}
+
+	return newverts;
+}
+
+void Image::drawv(const Matrix &t, const Vertex *v, GLsizei count, GLenum mode, const uint16 *e, GLsizei ecount) const
+{
+	bind();
+
+	glPushMatrix();
+
+	glMultMatrixf((const GLfloat *)t.getElements());
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+	// XXX: drawg() enables/disables GL_COLOR_ARRAY in order to use the color
+	//      defined in the geometry to draw itself.
+	//      if the drawing method below is changed to use something other than
+	//      glDrawArrays(), drawg() needs to be updated accordingly!
+	glVertexPointer(2, GL_FLOAT, sizeof(Vertex), (GLvoid *)&v[0].x);
+	glTexCoordPointer(2, GL_FLOAT, sizeof(Vertex), (GLvoid *)&v[0].s);
+
+	if (e != 0 && ecount > 0)
+		glDrawElements(mode, ecount, GL_UNSIGNED_SHORT, (GLvoid *) e);
+	else
+		glDrawArrays(mode, 0, count);
+
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
+
+	glPopMatrix();
+}
+
+void Image::setDefaultMipmapSharpness(float sharpness)
+{
+	defaultMipmapSharpness = sharpness;
+}
+
+float Image::getDefaultMipmapSharpness()
+{
+	return defaultMipmapSharpness;
+}
+
+void Image::setDefaultMipmapFilter(Image::FilterMode f)
+{
+	defaultMipmapFilter = f;
+}
+
+Image::FilterMode Image::getDefaultMipmapFilter()
+{
+	return defaultMipmapFilter;
+}
+
+bool Image::isCompressed() const
+{
+	return compressed;
+}
+
+GLenum Image::getCompressedFormat(image::CompressedData::Format format) const
+{
+	switch (format)
+	{
+	case image::CompressedData::FORMAT_DXT1:
+		return GL_COMPRESSED_RGB_S3TC_DXT1_EXT;
+	case image::CompressedData::FORMAT_DXT3:
+		return GL_COMPRESSED_RGBA_S3TC_DXT3_EXT;
+	case image::CompressedData::FORMAT_DXT5:
+		return GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
+	case image::CompressedData::FORMAT_BC4:
+		return GL_COMPRESSED_RED_RGTC1;
+	case image::CompressedData::FORMAT_BC4s:
+		return GL_COMPRESSED_SIGNED_RED_RGTC1;
+	case image::CompressedData::FORMAT_BC5:
+		return GL_COMPRESSED_RG_RGTC2;
+	case image::CompressedData::FORMAT_BC5s:
+		return GL_COMPRESSED_SIGNED_RG_RGTC2;
+	default:
+		return GL_RGBA8;
+	}
+}
+
+bool Image::hasNpot()
+{
+	return GLEE_VERSION_2_0 || GLEE_ARB_texture_non_power_of_two;
+}
+
+bool Image::hasAnisotropicFilteringSupport()
+{
+	return GLEE_EXT_texture_filter_anisotropic;
+}
+
+bool Image::hasMipmapSupport()
+{
+	return GLEE_VERSION_1_4 || GLEE_SGIS_generate_mipmap;
+}
+
+bool Image::hasMipmapSharpnessSupport()
+{
+	return GLEE_VERSION_1_4 || GLEE_EXT_texture_lod_bias;
+}
+
+bool Image::hasCompressedTextureSupport()
+{
+	return GLEE_VERSION_1_3 || GLEE_ARB_texture_compression;
+}
+
+bool Image::hasCompressedTextureSupport(image::CompressedData::Format format)
+{
+	if (!hasCompressedTextureSupport())
+		return false;
+
+	switch (format)
+	{
+	case image::CompressedData::FORMAT_DXT1:
+	case image::CompressedData::FORMAT_DXT3:
+	case image::CompressedData::FORMAT_DXT5:
+		return GLEE_EXT_texture_compression_s3tc;
+	case image::CompressedData::FORMAT_BC4:
+	case image::CompressedData::FORMAT_BC4s:
+	case image::CompressedData::FORMAT_BC5:
+	case image::CompressedData::FORMAT_BC5s:
+		return (GLEE_VERSION_3_0 || GLEE_ARB_texture_compression_rgtc || GLEE_EXT_texture_compression_rgtc);
+	default:
+		break;
+	}
+
+	return false;
+}
+
+} // opengl
+} // graphics
+} // love
---
+++ src/modules/window/wrap_Window.cpp
@@ -0,0 +1,318 @@
+/**
+ * Copyright (c) 2006-2013 LOVE Development Team
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ **/
+
+#include "wrap_Window.h"
+#include "sdl/Window.h"
+
+namespace love
+{
+namespace window
+{
+
+static Window *instance = 0;
+
+int w_getDisplayCount(lua_State *L)
+{
+	lua_pushinteger(L, instance->getDisplayCount());
+	return 1;
+}
+
+int w_setMode(lua_State *L)
+{
+	int w = luaL_checkint(L, 1);
+	int h = luaL_checkint(L, 2);
+
+	if (lua_isnoneornil(L, 3))
+	{
+		luax_pushboolean(L, instance->setWindow(w, h, 0));
+		return 1;
+	}
+
+	luaL_checktype(L, 3, LUA_TTABLE);
+
+	WindowFlags flags;
+
+	lua_getfield(L, 3, "fullscreentype");
+	if (!lua_isnoneornil(L, -1))
+	{
+		const char *typestr = luaL_checkstring(L, -1);
+
+		if (!Window::getConstant(typestr, flags.fstype))
+			return luaL_error(L, "Invalid fullscreen type: %s", typestr);
+	}
+	else
+	{
+		// Default to "normal" fullscreen.
+		flags.fstype = Window::FULLSCREEN_TYPE_NORMAL;
+	}
+	lua_pop(L, 1);
+
+	flags.fullscreen = luax_boolflag(L, 3, "fullscreen", false);
+	flags.vsync = luax_boolflag(L, 3, "vsync", true);
+	flags.fsaa = luax_intflag(L, 3, "fsaa", 0);
+	flags.resizable = luax_boolflag(L, 3, "resizable", false);
+	flags.minwidth = luax_intflag(L, 3, "minwidth", 1);
+	flags.minheight = luax_intflag(L, 3, "minheight", 1);
+	flags.borderless = luax_boolflag(L, 3, "borderless", false);
+	flags.centered = luax_boolflag(L, 3, "centered", true);
+	flags.display = luax_intflag(L, 3, "display", 1);
+
+	// Display index is 1-based in Lua and 0-based internally.
+	flags.display--;
+
+	EXCEPT_GUARD(luax_pushboolean(L, instance->setWindow(w, h, &flags));)
+	return 1;
+}
+
+int w_getMode(lua_State *L)
+{
+	int w, h;
+	WindowFlags flags;
+	instance->getWindow(w, h, flags);
+	lua_pushnumber(L, w);
+	lua_pushnumber(L, h);
+
+	lua_newtable(L);
+
+	const char *fstypestr = "normal";
+	Window::getConstant(flags.fstype, fstypestr);
+
+	lua_pushstring(L, fstypestr);
+	lua_setfield(L, -2, "fullscreentype");
+
+	luax_pushboolean(L, flags.fullscreen);
+	lua_setfield(L, -2, "fullscreen");
+
+	luax_pushboolean(L, flags.vsync);
+	lua_setfield(L, -2, "vsync");
+
+	lua_pushinteger(L, flags.fsaa);
+	lua_setfield(L, -2, "fsaa");
+
+	luax_pushboolean(L, flags.resizable);
+	lua_setfield(L, -2, "resizable");
+
+	lua_pushinteger(L, flags.minwidth);
+	lua_setfield(L, -2, "minwidth");
+
+	lua_pushinteger(L, flags.minheight);
+	lua_setfield(L, -2, "minheight");
+
+	luax_pushboolean(L, flags.borderless);
+	lua_setfield(L, -2, "borderless");
+
+	luax_pushboolean(L, flags.centered);
+	lua_setfield(L, -2, "centered");
+
+	// Display index is 0-based internally and 1-based in Lua.
+	lua_pushinteger(L, flags.display + 1);
+	lua_setfield(L, -2, "display");
+
+	return 3;
+}
+
+int w_getFullscreenModes(lua_State *L)
+{
+	int displayindex = luaL_optint(L, 1, 1) - 1;
+
+	std::vector<Window::WindowSize> modes = instance->getFullscreenSizes(displayindex);
+
+	lua_createtable(L, modes.size(), 0);
+
+	for (size_t i = 0; i < modes.size(); i++)
+	{
+		lua_pushinteger(L, i+1);
+		lua_createtable(L, 0, 2);
+
+		// Inner table attribs.
+
+		lua_pushinteger(L, modes[i].width);
+		lua_setfield(L, -2, "width");
+
+		lua_pushinteger(L, modes[i].height);
+		lua_setfield(L, -2, "height");
+
+		// Inner table attribs end.
+
+		lua_settable(L, -3);
+	}
+
+	return 1;
+}
+
+int w_setFullscreen(lua_State *L)
+{
+	bool fullscreen = luax_toboolean(L, 1);
+	Window::FullscreenType fstype = Window::FULLSCREEN_TYPE_MAX_ENUM;
+
+	const char *typestr = lua_isnoneornil(L, 2) ? 0 : luaL_checkstring(L, 2);
+	if (typestr && !Window::getConstant(typestr, fstype))
+		return luaL_error(L, "Invalid fullscreen type: %s", typestr);
+
+	bool success = false;
+	if (fstype == Window::FULLSCREEN_TYPE_MAX_ENUM)
+		success = instance->setFullscreen(fullscreen);
+	else
+		success = instance->setFullscreen(fullscreen, fstype);
+
+	luax_pushboolean(L, success);
+	return 1;
+}
+
+int w_getFullscreen(lua_State *L)
+{
+	int w, h;
+	WindowFlags flags;
+	instance->getWindow(w, h, flags);
+
+	const char *typestr;
+	if (!Window::getConstant(flags.fstype, typestr))
+		luaL_error(L, "Unknown fullscreen type.");
+
+	luax_pushboolean(L, flags.fullscreen);
+	lua_pushstring(L, typestr);
+	return 2;
+}
+
+int w_isCreated(lua_State *L)
+{
+	luax_pushboolean(L, instance->isCreated());
+	return 1;
+}
+
+int w_getWidth(lua_State *L)
+{
+	lua_pushinteger(L, instance->getWidth());
+	return 1;
+}
+
+int w_getHeight(lua_State *L)
+{
+	lua_pushinteger(L, instance->getHeight());
+	return 1;
+}
+
+int w_getDimensions(lua_State *L)
+{
+	lua_pushinteger(L, instance->getWidth());
+	lua_pushinteger(L, instance->getHeight());
+	return 2;
+}
+
+int w_getDesktopDimensions(lua_State *L)
+{
+	int width = 0, height = 0;
+	int displayindex = luaL_optint(L, 1, 1) - 1;
+	instance->getDesktopDimensions(displayindex, width, height);
+	lua_pushinteger(L, width);
+	lua_pushinteger(L, height);
+	return 2;
+}
+
+int w_setIcon(lua_State *L)
+{
+	image::ImageData *i = luax_checktype<image::ImageData>(L, 1, "ImageData", IMAGE_IMAGE_DATA_T);
+	luax_pushboolean(L, instance->setIcon(i));
+	return 1;
+}
+
+int w_getIcon(lua_State *L)
+{
+	image::ImageData *i = instance->getIcon();
+	if (i)
+	{
+		i->retain();
+		luax_pushtype(L, "ImageData", IMAGE_IMAGE_DATA_T, i);
+	}
+	else
+		lua_pushnil(L);
+	return 1;
+}
+
+int w_setTitle(lua_State *L)
+{
+	std::string title = luax_checkstring(L, 1);
+	instance->setWindowTitle(title);
+	return 0;
+}
+
+int w_getTitle(lua_State *L)
+{
+	luax_pushstring(L, instance->getWindowTitle());
+	return 1;
+}
+
+int w_hasFocus(lua_State *L)
+{
+	luax_pushboolean(L, instance->hasFocus());
+	return 1;
+}
+
+int w_hasMouseFocus(lua_State *L)
+{
+	luax_pushboolean(L, instance->hasMouseFocus());
+	return 1;
+}
+
+int w_isVisible(lua_State *L)
+{
+	luax_pushboolean(L, instance->isVisible());
+	return 1;
+}
+
+static const luaL_Reg functions[] =
+{
+	{ "getDisplayCount", w_getDisplayCount },
+	{ "setMode", w_setMode },
+	{ "getMode", w_getMode },
+	{ "getFullscreenModes", w_getFullscreenModes },
+	{ "setFullscreen", w_setFullscreen },
+	{ "getFullscreen", w_getFullscreen },
+	{ "isCreated", w_isCreated },
+	{ "getWidth", w_getWidth },
+	{ "getHeight", w_getHeight },
+	{ "getDimensions", w_getDimensions },
+	{ "getDesktopDimensions", w_getDesktopDimensions },
+	{ "setIcon", w_setIcon },
+	{ "getIcon", w_getIcon },
+	{ "setTitle", w_setTitle },
+	{ "getTitle", w_getTitle },
+	{ "hasFocus", w_hasFocus },
+	{ "hasMouseFocus", w_hasMouseFocus },
+	{ "isVisible", w_isVisible },
+	{ 0, 0 }
+};
+
+extern "C" int luaopen_love_window(lua_State *L)
+{
+	EXCEPT_GUARD(instance = sdl::Window::createSingleton();)
+
+	WrappedModule w;
+	w.module = instance;
+	w.name = "window";
+	w.flags = MODULE_T;
+	w.functions = functions;
+	w.types = 0;
+
+	return luax_register_module(L, w);
+}
+
+} // window
+} // love
